589df5b4b9615eb62b125a10cba8a8272e6624c2
A2014 <19062010nastya2014@gmail.com>
2022-04-03 14:02:50 +0300

    added homework7

---
+++ homework7/homework7.py
@@ -0,0 +1,19 @@
+# task 2
+""" программа, позволяющая запись и чтение из файла .txt"""
+
+with open('users_data.txt', 'w') as users_data:
+
+    name = input("Enter your name: ") 
+    print(name)
+    age = input("Enter your age: ")
+    print(age)
+    city = input("Enter your city: ")
+    print(city)
+
+    data = f"{name}:{age}:{city}"
+    users_data.write(data)
+
+with open('my_file.txt', 'r') as user_file:
+    
+    for line in user_file:
+        print(line)
